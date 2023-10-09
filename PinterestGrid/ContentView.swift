b2186e0d8e29978cc7341f6623859bcd866004ee
Elaidzha1940 <eli080293@gmail.com>
2023-10-10 02:40:10 +0300

    Updated ContentView

--- PinterestGrid/ContentView.swift
+++ PinterestGrid/ContentView.swift
@@ -12,14 +12,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        var gridItem = [GridItem]()
-        
-        for i in 0 ..< 30 {
-            let randomHeight = CGFloat.random(in: 10)
-        }
-        
-        ScrollView {
-            PinterestGrid(gridItem: <#[GridItem]#>, numberofColumns: <#Int#>)
+        return ScrollView {
+            PinterestGrid(gridItem: gridItem, numberofColumns: 2, spacing: 20, horizontalPadding: 20)
             
         }
     }
