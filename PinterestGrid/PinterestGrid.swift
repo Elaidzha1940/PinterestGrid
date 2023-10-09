bece9b9483c91a053422f154c702754c91b1409e
Elaidzha1940 <eli080293@gmail.com>
2023-10-10 02:16:08 +0300

    init()

--- PinterestGrid/PinterestGrid.swift
+++ PinterestGrid/PinterestGrid.swift
@@ -12,6 +12,7 @@ import SwiftUI
 struct GridItem: Identifiable {
     let id = UUID()
     let height: CGFloat
+    let title: String
 }
 
 struct PinterestGrid: View {
@@ -21,7 +22,17 @@ struct PinterestGrid: View {
     }
     let columns = [
         Column(gridItems: [
-        GridItem(height: 200)
+            GridItem(height: 200, title: "1"),
+            GridItem(height: 170, title: "5"),
+            GridItem(height: 300, title: "8"),
+            GridItem(height: 150, title: "4"),
+        ]),
+        
+        Column(gridItems: [
+            GridItem(height: 100, title: "2"),
+            GridItem(height: 350, title: "7"),
+            GridItem(height: 150, title: "6"),
+            GridItem(height: 250, title: "3"),
         ])
     ]
     
@@ -32,19 +43,18 @@ struct PinterestGrid: View {
         
         HStack(alignment: .top, spacing: spacing) {
              
-            LazyVStack(spacing: spacing) {
-                ForEach(0..<17) { _ in
-                    Rectangle()
-                        .foregroundColor(.black)
-                        .frame(height: CGFloat.random(in: 20...250))
-                }
-            }
-            
-            LazyVStack(spacing: spacing) {
-                ForEach(0..<17) { _ in
-                    Rectangle()
-                        .foregroundColor(.black)
-                        .frame(height: CGFloat.random(in: 20...250))
+            ForEach(columns) { column in
+                LazyVStack(spacing: spacing) {
+                    ForEach(column.gridItems) { gridItem in
+                        
+                        Rectangle()
+                            .foregroundColor(.mint)
+                            .frame(height: gridItem.height)
+                            .overlay(
+                                Text(gridItem.title)
+                                    .font(.system(size: 20, weight: .bold, design: .rounded))
+                            )
+                    }
                 }
             }
         }
