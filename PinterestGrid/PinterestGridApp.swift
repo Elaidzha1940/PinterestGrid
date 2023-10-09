e941d9b671584dd81863516e972aa544f6fbe861
Elaidzha1940 <eli080293@gmail.com>
2023-10-09 19:25:29 +0300

    PinterestGrid

---
+++ PinterestGrid/PinterestGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestGridApp.swift
+//  PinterestGrid
+//
+//  Created by Elaidzha Shchukin on 09.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinterestGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
